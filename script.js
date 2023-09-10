872bfd0b654cfbe4793ff8855d6d62d9a21c5cf0
jerrisneh <jerrisneh@gmail.com>
2023-09-10 15:22:45 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+let week = document.getElementById("currentWeek");
+let time = document.getElementById("time")
+
+function getCurrentDayOfWeek(){
+    const weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    const currentDate = new Date();
+    const weekDay = weekDays[currentDate.getUTCDay()];
+    return weekDay;
+}
+
+function getCurrentUTCTime(){
+    const currentTime = Date.now()
+    const formattedTime = new Date(currentTime).toLocaleString();
+    return formattedTime;
+}
+
+week.textConent = getCurrentDayOfWeek();
+time.textContent = getCurrentUTCTime();
